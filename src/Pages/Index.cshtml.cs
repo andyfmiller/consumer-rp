45904cfff01dac6d8ce5954ccbf934a77da012ee
Andy Miller <andy@andyfmiller.com>
2018-01-09 22:28:18 -0800

    Fill in landing page.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Threading.Tasks;
+using Consumer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Consumer.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IndexModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public void OnGet()
         {
-
         }
+
+        public async Task<IActionResult> OnPostAsync(string username)
+        {
+            await _signInManager.PasswordSignInAsync(username, "Password1!", false, lockoutOnFailure: false);
+
+            return RedirectToPage("./Index");
+        }
+
     }
 }
